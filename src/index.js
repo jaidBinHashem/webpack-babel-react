4554f71ad5cfb8115c8802d6b982371e38a7ba5d
Md. Jaid Bin Hashem <mdjaidbinhashem34@gmail.com>
2018-03-09 10:18:08 +0600

    Common React Project Skeleton

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <h1>{title}</h1>,
+  document.getElementById('app')
+);
+
+
+module.hot.accept();
